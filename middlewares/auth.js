99eff3ca0b271c6626d23b67926d34920000bea7
Uduak Edet <edet.uduak@techvibesltd.com>
2023-05-24 19:28:32 +0100

    token in headers

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,10 +6,10 @@ import catchAsyncErrors from "./catchAsyncError.js";
 
 // Check if user is authenticated
 export const isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
-  const { token } = req.cookies;
+  // const { token } = req.cookies;
+  const token = req.headers.authorization.split(" ")[1];
   if (!token) {
-    res.status(401).json({ message: "Please log in to access this resource."})
-
+    res.status(401).json({ message: "Please log in to access this resource." });
   }
   const decoded = Jwt.verify(token, process.env.JWT_SECRET);
   req.user = await User.findById(decoded.id);
@@ -18,9 +18,12 @@ export const isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
 
 export const isAuthenticatedEnumerator = catchAsyncErrors(
   async (req, res, next) => {
-    const { token } = req.cookies;
+    // const { token } = req.cookies;
+    const token = req.headers.authorization.split(" ")[1];
     if (!token) {
-      res.status(401).json({ message: "Please log in to access this resource."})
+      res
+        .status(401)
+        .json({ message: "Please log in to access this resource." });
     }
     const decoded = Jwt.verify(token, process.env.JWT_SECRET);
     req.enumerator = await Enumerator.findById(decoded.id);
@@ -32,7 +35,9 @@ export const isAuthenticatedEnumerator = catchAsyncErrors(
 export const authorizeRoles = (...roles) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
-      res.status(403).json({ message: `${req.user.role} is not authorized to access this resource`})
+      res.status(403).json({
+        message: `${req.user.role} is not authorized to access this resource`,
+      });
     }
     next();
   };
