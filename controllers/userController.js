981672c7124b0349cc95036b704780919af83920
Jesse Abuaja <jesseabuaja@hotmail.com>
2023-06-12 08:42:48 +0100

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -281,7 +281,7 @@ export const loginUser = async (req, res) => {
       const token = passwordMatch && sendToken(enumerator);
 
       if (passwordMatch) {
-        res.status(200).json({ enumerator, token });
+        res.status(200).json({ user: enumerator, token });
       } else {
         res.status(401).json({
           success: false,
