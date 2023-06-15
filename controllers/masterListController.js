407b96f3a23b79657313671b61fcaf3f412f40f0
Uduak Edet <edet.uduak@techvibesltd.com>
2023-06-15 17:52:05 +0100

    update

--- controllers/masterListController.js
+++ controllers/masterListController.js
@@ -9,7 +9,7 @@ oneMonthAgo.setMonth(oneMonthAgo.getMonth() - 1);
 
 export const getMasterListData = async (req, res) => {
   try {
-    const { startDateFilter, endDateFilter } = req.query;
+    const { startDateFilter, endDateFilter, page } = req.query;
     const query = {};
     const startDate = startDateFilter ? startDateFilter : oneMonthAgo;
     const endDate = endDateFilter ? endDateFilter : today;
@@ -21,7 +21,8 @@ export const getMasterListData = async (req, res) => {
     if (req?.user?.role === "admin" || req?.user?.role === "super_admin") {
       query.approved = 1;
     }
-    console.log(startDate, endDate);
+    const currentPage = page || 1;
+    const skip = (currentPage - 1) * 20;
     query.created_at = { $gte: startDate, $lte: endDate };
     // const query = {
     //   $and: [
@@ -38,7 +39,8 @@ export const getMasterListData = async (req, res) => {
     // };
 
     // Query the database for the desired forms
-    Form.find(query)
+    const total = await Form.countDocuments(query);
+    const forms = await Form.find(query)
       .sort({ created_at: -1 }) // Sort by created_at in descending order
       .populate("foodItems", "name brand price") // Populate foodItems and select the name, brand, and price fields
       .populate("accomodations", "type rooms price") // Populate accomodations and select the name, propertyType, and price fields
@@ -51,13 +53,15 @@ export const getMasterListData = async (req, res) => {
         "government_project comment_for_government_project crime_report comment_for_crime_report accidents comment_for_accidents note"
       )
       .populate("created_by") // Populate created_by and select the name and _id fields
-      .exec((err, forms) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-        res.status(200).json(forms);
-      });
+      .skip(skip)
+      .limit(20);
+    // .exec((err, forms) => {
+    //   if (err) {
+    //     console.error(err);
+    //     return;
+    //   }
+    // });
+    res.status(200).json({ forms, total });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
